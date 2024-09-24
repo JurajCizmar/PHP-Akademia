99af106754a32113990e68d7d22bfaa00ad3546b
Juraj Čižmár <100305420+JurajCizmar@users.noreply.github.com>
2024-09-24 15:55:28 +0200

    added name acquisition through URL
    now name can be submitted through URL and POST method simultaneously

--- index.php
+++ index.php
@@ -13,22 +13,33 @@ declare(strict_types=1);
 <body>
 
 <form action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method = "post">
-    <button
-        name = "attendanceButton">
-        Attendance
-    </button>
-    <button 
-        name="resetButton">
-        Reset
-    </button>
+    <label 
+        for="name">
+        Student's name:
+    </label><br>
+    <input 
+        type="text" 
+        id="name" 
+        name="name">
+    <br>
+    <input 
+        type="submit" 
+        name="submit" 
+        value="Submit">
+    <input 
+        type="submit" 
+        name="resetButton" 
+        value="Reset">
 </form>
 
 <pre>
 <?php
 
-function writetolog(string $fileName, string $content, bool $isLate){
+function writetolog(string $fileName, string $studentsName, string $content, bool $isLate){
 
-    $content .= $isLate? ", student is late." : "";
+
+
+    $content .= $isLate? ", " . $studentsName . " is late." : " " . $studentsName;
     $content .= "\n";
     
     file_put_contents($fileName, $content, FILE_APPEND);
@@ -44,40 +55,58 @@ function resetlog(string $fileName){
     file_put_contents($fileName, "");
 }
 
-
 $fileDate = date("l d.m.Y H:i:s");
 $displayDate = date('l jS \of F Y H:i:s A');
-$file = "TimeLog.txt";
+$fileName = "TimeLog.txt";
+$errors = false;
 
 echo "<hr>" . "Hello :)" . "\n";
 echo "Today is " . $displayDate . "<hr>";
 
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['attendanceButton'])){
+if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-    $hour = date("H");
+    if (isset($_POST["submit"]) && !empty($_POST["name"])) {
+        
+        $studentsName = htmlspecialchars($_POST["name"]);
+        $hour = date("H");
+        $isLate = ($hour >= 8 && $hour < 20) ? true : false ;
 
-    if ($hour >= 8 && $hour < 20){
-        $isLate = true;
-    } else {
-        $isLate = false;
+        if ($hour >= 20){
+            die("Attendance at this time is not possible :| ");
+
+        } else {
+            writetolog($fileName, $studentsName, $fileDate, $isLate);
+        }
+
+    } elseif (isset($_POST["submit"]) && empty($_POST["name"])){
+        echo "Fill in the student's name please.";
+        echo "<br>";
+        $errors = true;
+
+    } elseif (isset($_POST["resetButton"])){
+        resetlog($fileName);
     }
 
-    if ($hour >= 20){
+} elseif ($_SERVER["REQUEST_METHOD"] == "GET" && !empty($_GET["name"])){
 
+    $studentsName = htmlspecialchars($_GET["name"]);
+    
+    $hour = date("H");
+    $isLate = ($hour >= 8 && $hour < 20) ? true : false ;
+
+    if ($hour >= 20){
         die("Attendance at this time is not possible :| ");
 
     } else {
-        writetolog($file, $fileDate, $isLate);
+        writetolog($fileName, $studentsName, $fileDate, $isLate);
     }
-
-} elseif ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['resetButton'])){
-    resetlog($file);
+    
 }
 
-if (file_exists($file)){
-    $fileContent = getlog($file);
-    echo $fileContent; 
+if (file_exists($fileName) && !$errors){
+    $fileContent = getlog($fileName);
+    echo $fileContent;
 }
 
 ?>
